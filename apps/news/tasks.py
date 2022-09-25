aa916ca93f430901569185c17578b5a391b20c26
Vitaly <vitalikmok@gmail.com>
2022-09-25 19:44:05 +0300

    Initial

---
+++ apps/news/tasks.py
@@ -0,0 +1,20 @@
+from news_management_backend.celery import app
+from .services import (
+    _get_links_to_news_from_yandex,
+    _get_links_to_news_from_ozon,
+    _parsing_news_yandex,
+    _parsing_news_ozon,
+)
+
+
+@app.task
+def parsing_news_from_yandex():
+    """ Парсинг новостей с яндекса """
+    links_yandex_news = _get_links_to_news_from_yandex()
+    _parsing_news_yandex(links_yandex_news)
+
+@app.task
+def parsing_news_from_ozon():
+    """ Парсинг новостей с озона """
+    links_ozon_news, dates = _get_links_to_news_from_ozon()
+    _parsing_news_ozon(links_ozon_news, dates)
